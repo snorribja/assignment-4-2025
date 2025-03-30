c22cfeaf24f07a38ec3a49ab6b78b32794e4e723
snorribja <snorribjarka@gmail.com>
2025-03-30 18:07:08 +0000

    Datadog stuff for assignment 6

---
+++ config/tracing.ts
@@ -0,0 +1,10 @@
+import tracer from 'dd-trace';
+import StatsD from 'hot-shots';
+
+tracer.init({service: process.env.DD_SERVICE_NAME || 'assignment-4-2025', env: process.env.NODE_ENV || 'development', version: process.env.VERSION || '1.0.0',});
+
+const dogstatsd = new StatsD();
+
+dogstatsd.increment('page.views');
+
+export { tracer, dogstatsd };
